b78dbf738e9b0359f4880ad31fb2efc307999b86
Berenger Desgardin <berenger.desgardin@gmail.com>
2018-06-14 17:23:28 +0200

    Push pour merge

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,24 +1,51 @@
-import {messages} from './messages'
 import axios from 'axios'
-import {donnees} from './getNews'
-
-console.log('App.js loaded')
 
 /*
+import {messages} from './messages'
 var articles = ''
 for (var i = 0; i< messages.length; i++){
     articles += `Message n° ${i} <br />
     author : ${messages[i].author} <br />
-     message : ${messages[i].message} 
+     message : ${messages[i].message}
     <br /><br />`
 }
 document.getElementById("message").innerHTML=articles
 */
+var param = location.search
 
-axios.get('https://newsapi.org/v2/top-headlines?' +
-    'sources=reddit-r-all&' +
-    'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
-    .then((HttpResponse)=>{
+if (param){
+    param = param.substr(1, 1)
+    axios.get('https://newsapi.org/v2/top-headlines?' +
+        'sources=reddit-r-all&' +
+        'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
+        .then((HttpResponse)=>{
+
+                var titre=`${HttpResponse.data.articles[param].title}`
+                var auteur = `${HttpResponse.data.articles[param].author}`
+                var description = `${HttpResponse.data.articles[param].description}`
+                var url = `${HttpResponse.data.articles[param].url}`
+                var urlImage = `${HttpResponse.data.articles[param].urlToImage}`
+                var datePublication = `${HttpResponse.data.articles[param].publishedAt}`
+
+                document.getElementById("row").innerHTML=`
+                <article id="redditArticle${param}" class="col-sm-12" >
+                   <h3 class='text-center'>${titre}</h3> <br>
+                   <i> Posté par ${auteur} </i> <br>
+                   ${description}<br>
+                   <img src="${urlImage}" style="max-height: 300px; max-width=20%" >  <br>
+                   <div ><a type="button" class="btn btn-primary" href="${url}">Voir plus</a>  Publié il y a  ${datePublication} <br> </div>
+                   <a type="button" class="btn btn-primary" href="index.html">Retourner à l'accueil</a>
+                </article>`
+
+
+            }
+        )
+}
+else{
+    axios.get('https://newsapi.org/v2/top-headlines?' +
+        'sources=reddit-r-all&' +
+        'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
+        .then((HttpResponse)=>{
             for (var i = 0; i < HttpResponse.data.totalResults; i++){
                 var titre=`${HttpResponse.data.articles[i].title}`
                 var auteur = `${HttpResponse.data.articles[i].author}`
@@ -28,61 +55,50 @@ axios.get('https://newsapi.org/v2/top-headlines?' +
                 var datePublication = `${HttpResponse.data.articles[i].publishedAt}`
 
                 document.getElementById("row").innerHTML+=
+                    `
+            <div class="col-12 col-lg-4" >
+                <div class="article" id="redditArticle${i}">
+                    <div class="row">
+                        <div class="col-12 titre">
+                            <h3 class='text-center'>${titre}</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 author">
+                            Posté par ${auteur}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 description">
+                            ${description}
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 image">
+                            <img src="${urlImage}" class="img-fluid">
+                        </div>
+                        <div>
+                            <button type="button" class="btn btn-primary" href="${url}">Voir plus</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 published">
+                            Publié il y a - ${datePublication}
+                        </div>
+                    </div>
+                </div>                    
+            </div>
+                    
+                    
+                    
+                    
+                    
 `
-<div class="col-12 col-lg-4" >
-    <div class="article" id="redditArticle${i}">
-        <div class="row">
-            <div class="col-12 titre">
-                <h3 class='text-center'>${titre}</h3>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 author">
-                Posté par ${auteur}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 description">
-                ${description}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 image">
-                <img src="${urlImage}" class="img-fluid">
-            </div>
-            <div>
-                <button type="button" class="btn btn-primary" href="${url}">Voir plus</button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 published">
-                Publié il y a - ${datePublication}
-            </div>
-        </div>
-    </div>                    
-</div>
+                }
 
-                    
-                    
-                    
-                    
-                    
-`
             }
-
-        }
-    )
-
-
-
-/*
- document.getElementById("redditArt").innerHTML=`${HttpResponse.data.articles[1].author} ${HttpResponse.data.articles[1].title}`
-for (var j = 0; j< redditArticle.length ; j++){
-    console.log('coucou')
-    axios.get('https://newsapi.org/v2/top-headlines?' +
-        'sources=reddit-r-all&' +
-        'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
-        .then((HttpResponse)=> document.getElementById("redditArt").innerHTML=HttpResponse.data.articles[j])
-
+        )
 }
-*/
+
+
+
--- front-end/src/getNews.js
+++ front-end/src/getNews.js
@@ -1,12 +1,59 @@
-import axios from 'axios'
+import axios from "axios/index";
 
-var donnees = new XMLHttpRequest();
-donnees.open("POST", '/server', true);
-donnees.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+var param = location.search
 
-axios.get('https://newsapi.org/v2/top-headlines?' +
-    'sources=reddit-r-all&' +
-    'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
-    .then((HttpResponse)=> donnees.send(HttpResponse.data))
+if (param){
+    param = param.substr(1, 1)
+    axios.get('https://newsapi.org/v2/top-headlines?' +
+        'sources=reddit-r-all&' +
+        'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
+        .then((HttpResponse)=>{
 
-export { donnees }
+                var titre=`${HttpResponse.data.articles[param].title}`
+                var auteur = `${HttpResponse.data.articles[param].author}`
+                var description = `${HttpResponse.data.articles[param].description}`
+                var url = `${HttpResponse.data.articles[param].url}`
+                var urlImage = `${HttpResponse.data.articles[param].urlToImage}`
+                var datePublication = `${HttpResponse.data.articles[param].publishedAt}`
+
+                document.getElementById("row").innerHTML=`
+                <article id="redditArticle${param}" class="col-sm-12" >
+                   <h3 class='text-center'>${titre}</h3> <br>
+                   <i> Posté par ${auteur} </i> <br>
+                   ${description}<br>
+                   <img src="${urlImage}" style="max-height: 300px; max-width=20%" >  <br>
+                   <div ><a type="button" class="btn btn-primary" href="${url}">Voir plus</a>  Publié il y a  ${datePublication} <br> </div>
+                   <a type="button" class="btn btn-primary" href="index.html">Retourner à l'accueil</a>
+                </article>`
+
+
+            }
+        )
+}
+else{
+    axios.get('https://newsapi.org/v2/top-headlines?' +
+        'sources=reddit-r-all&' +
+        'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
+        .then((HttpResponse)=>{
+                for (var i = 0; i < HttpResponse.data.totalResults; i++){
+                    var titre=`${HttpResponse.data.articles[i].title}`
+                    var auteur = `${HttpResponse.data.articles[i].author}`
+                    var description = `${HttpResponse.data.articles[i].description}`
+                    var url = `${HttpResponse.data.articles[i].url}`
+                    var urlImage = `${HttpResponse.data.articles[i].urlToImage}`
+                    var datePublication = `${HttpResponse.data.articles[i].publishedAt}`
+
+                    document.getElementById("row").innerHTML+=`
+                    <article id="redditArticle${i}" class="col-sm-6" style="border-style: solid; border-width: 1px;">
+                       <h3 class='text-center'>${titre}</h3> <br>
+                       <i> Posté par ${auteur} </i> <br>
+                       ${description}<br>
+                       <img src="${urlImage}" style="max-height: 300px; max-width=20%" >  <br>
+                       <div style="position: absolute;bottom: 0;"><a type="button" class="btn btn-info" href="index.html?${i}">Voir l\'article</a>
+                       <a type="button" class="btn btn-primary" href="${url}">Voir plus</a>  Publié il y a  ${datePublication} <br> </div>
+                    </article>`
+                }
+
+            }
+        )
+}
