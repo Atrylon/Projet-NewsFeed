7d893b91e22fcb8d483aaaa7d3f5afddbff58153
nicolas <nicolaslauwick@msn.com>
2018-06-14 17:12:59 +0200

    nl

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -19,26 +19,59 @@ axios.get('https://newsapi.org/v2/top-headlines?' +
     'sources=reddit-r-all&' +
     'apiKey=591d3c16f1ab4f129bcd48a5b4480317')
     .then((HttpResponse)=>{
-        for (var i = 0; i < HttpResponse.data.totalResults; i++){
-            var titre=`${HttpResponse.data.articles[i].title}`
-            var auteur = `${HttpResponse.data.articles[i].author}`
-            var description = `${HttpResponse.data.articles[i].description}`
-            var url = `${HttpResponse.data.articles[i].url}`
-            var urlImage = `${HttpResponse.data.articles[i].urlToImage}`
-            var datePublication = `${HttpResponse.data.articles[i].publishedAt}`
+            for (var i = 0; i < HttpResponse.data.totalResults; i++){
+                var titre=`${HttpResponse.data.articles[i].title}`
+                var auteur = `${HttpResponse.data.articles[i].author}`
+                var description = `${HttpResponse.data.articles[i].description}`
+                var url = `${HttpResponse.data.articles[i].url}`
+                var urlImage = `${HttpResponse.data.articles[i].urlToImage}`
+                var datePublication = `${HttpResponse.data.articles[i].publishedAt}`
 
-            document.getElementById("row").innerHTML+=`
-                    <article id="redditArticle${i}" class="col-sm-6" style="border-style: solid; border-width: 1px;">
-                       <h3 class='text-center'>${titre}</h3> <br>
-                       <i> Posté par ${auteur} </i> <br>
-                       ${description}<br>
-                       <img src="${urlImage}" style="max-height: 300px; max-width=20%" >  <br>
-                       <div style="position: absolute;bottom: 0;"><button type="button" class="btn btn-primary" href="${url}">Voir plus</button>  Publié il y a  ${datePublication} <br> </div>
-                    </article>`
+                document.getElementById("row").innerHTML+=
+`
+<div class="col-12 col-lg-4" >
+    <div class="article" id="redditArticle${i}">
+        <div class="row">
+            <div class="col-12 titre">
+                <h3 class='text-center'>${titre}</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 author">
+                Posté par ${auteur}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 description">
+                ${description}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 image">
+                <img src="${urlImage}" class="img-fluid">
+            </div>
+            <div>
+                <button type="button" class="btn btn-primary" href="${url}">Voir plus</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 published">
+                Publié il y a - ${datePublication}
+            </div>
+        </div>
+    </div>                    
+</div>
+
+                    
+                    
+                    
+                    
+                    
+`
+            }
+
         }
-
-    }
-)
+    )
 
 
 
